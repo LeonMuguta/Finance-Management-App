63d6cf8e9515889b7e5b8f4432db3554bc7c0f73
Tadiwa Muguta <leonmuguta7@gmail.com>
2024-10-24 11:49:56 +0200

    Added all application.properties files to .gitignore and chaged email schedule time to 8am

--- src/main/java/com/finance/financial_management_app/security/EmailService.java
+++ src/main/java/com/finance/financial_management_app/security/EmailService.java
@@ -2,7 +2,6 @@ package com.finance.financial_management_app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSendException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
